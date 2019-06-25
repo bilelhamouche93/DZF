04b847a417c73df524345676f106c26cc40ec780
Bilel Hamouche <bilelhamouche93@gmail.com>
2019-06-25 22:27:24 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,13 @@
 var g = document.getElementsByClassName("viewBtn");
-for (var i = 0, len = g.length; i < len; i++)
-{
+
+for (var i = 0, len = g.length; i < len; i++){
     (function(index){
         g[i].onclick = function(){
-              // alert(index) ;
-              var viewBtn = document.getElementsByClassName("viewBtn")[index];
-              var imgsrc = document.getElementsByClassName("viewBtn")[index].parentElement.parentElement.parentElement.parentElement.querySelector("img").getAttribute("src");
-              console.log(imgsrc)
-              var newtab= window.open(imgsrc, '_blank');
-              newtab.focus();
+            var imgsrc = document.getElementsByClassName("viewBtn")[index].parentElement.parentElement.parentElement.parentElement.querySelector("img").getAttribute("src");
+            var newtab= window.open(imgsrc, '_blank');
+            newtab.focus();
         }
     })(i);
 }
+
+
