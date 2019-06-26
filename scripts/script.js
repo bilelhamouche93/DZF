ce1c2b7e53fa38078e475aa2f24cfdd17aa3e96b
Bilel Hamouche <bilelhamouche93@gmail.com>
2019-06-26 14:26:34 +0100

    JavaScript refactoring

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,7 @@
-var g = document.getElementsByClassName("viewBtn");
-
-for (var i = 0, len = g.length; i < len; i++){
-    (function(index){
-        g[i].onclick = function(){
-            var imgsrc = document.getElementsByClassName("viewBtn")[index].parentElement.parentElement.parentElement.parentElement.querySelector("img").getAttribute("src");
-            var newtab= window.open(imgsrc, '_blank');
-            newtab.focus();
-        }
-    })(i);
-}
-
-
+var handlers = {
+  viewImg: function(id){
+    var imgsrc = id.parentElement.parentElement.parentElement.parentElement.querySelector("img").getAttribute("src");
+    var newtab= window.open(imgsrc, '_blank');
+    newtab.focus();    
+  }
+};
